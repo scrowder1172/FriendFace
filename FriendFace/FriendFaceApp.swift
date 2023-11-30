27d44425f36dc6abae0c62277e98a74a88d7bfbe
Scott Crowder <scrowder@gmail.com>
2023-11-30 13:35:37 -0600

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by SCOTT CROWDER on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
