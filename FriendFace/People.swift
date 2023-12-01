10e2cd8dbdef0164640b121711b477e480c6d69f
Scott Crowder <scrowder@gmail.com>
2023-12-01 09:57:46 -0600

    build relationships and add fields

--- FriendFace/People.swift
+++ FriendFace/People.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftData
 
 struct People: Codable, Identifiable {
     var id: String
@@ -15,13 +16,33 @@ struct People: Codable, Identifiable {
     var age: Int
     var company: String
     var about: String
+    var tags: [String]
+    var isActive: Bool
+    
+    @Relationship(deleteRule: .cascade) var friends: [Friends]? = [Friends]()
+    
+    var unwrappedFriends: [Friends] {
+        friends ?? []
+    }
 }
 
 struct Tags: Codable {
+    enum CodingKeys: String, CodingKey {
+        case tag = "tags"
+    }
+    
     var tag: String
 }
 
-struct Friends: Codable {
+struct Friends: Codable, Identifiable {
     var id: String
     var name: String
+    var person: People?
+    
+    init(id: String, name: String, person: People? = nil) {
+        self.id = id
+        self.name = name
+        self.person = person
+    }
+    
 }
